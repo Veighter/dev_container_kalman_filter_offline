73409a14d754e54229d56c312c2fec55b6ccfb6e
Veighter <59057919+Veighter@users.noreply.github.com>
2024-03-07 10:57:21 +0000

    Update data representation to one file each sensor

--- main.cc
+++ main.cc
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <fstream>
+
+// 8851 Daten sind da drin vorhanden
+// In welcher Einheit sind die Sekunden angegeben? Laut Abgabe in Microsekunden
 
 int main()
 {
-    std::cout << "Hello Dev Container";
+    std::ifstream data{""};
     return 0;
 }
