1009479a50f61846a6503336715e25a820eb5451
Veighter <59057919+Veighter@users.noreply.github.com>
2024-03-08 10:14:35 +0000

    Full Path necessary

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <math.h>
 // 8851 Daten sind da drin vorhanden
 // In welcher Einheit sind die Sekunden angegeben? Laut Abgabe in Microsekunden
 // alle 2 sekunden kommt ein GPS rein
@@ -29,7 +30,7 @@ int main()
 
     // read in of datafiles with the coloum structure:
     // Time [us]	ACC_X [mg]	ACC_Y [mg]	ACC_Z [mg]	GYRO_X [dps]	GYRO_Y [dps]	GYRO_Z [dps]	MAG_X [uT]	MAG_Y [uT]	MAG_Z [uT]
-    std::string data_IMU_path{"/old_data_from_sd/IMU_"};
+    std::string data_IMU_path{"/workspaces/dev_container_kalman_filter_offline/old_data_from_sd/IMU_"};
 
     imu_1.port = 0;
     imu_2.port = 1;
@@ -38,11 +39,11 @@ int main()
 
     IMU imus[4] = {imu_1, imu_2, imu_3, imu_4};
 
-    int i = 0; // Index for the array of values
+    int coloumn = 0; // Index for the array of values
     int row = 0;
     size_t pos = 0; // position of the delimiter in string
     std::string data{};
-    std::string delimiter = "\t";
+    std::string delimiter{};
     std::string path_ending{};
 
     for (IMU imu : imus)
@@ -60,51 +61,62 @@ int main()
 
         for (std::string values; std::getline(data_IMU, values) && row < DATA_ROWS; row++)
         {
-            i = 0;
-            while ((pos = values.find(delimiter)) != std::string::npos)
+            coloumn = 0;
+            while (coloumn < 10)
             {
+                if (coloumn != 9)
+                {
+                    delimiter = "\t";
+                }
+                else
+                {
+                    delimiter = "\r";
+                }
+                pos = values.find(delimiter);
+
                 data = values.substr(0, pos);
-                if (i == 0)
+                if (coloumn == 0)
                 {
                     imu.timeData[row] = std::stof(data);
                 }
-                if (i == 1)
+                if (coloumn == 1)
                 {
                     imu.accData[row].x = std::stof(data);
                 }
-                if (i == 2)
+                if (coloumn == 2)
                 {
                     imu.accData[row].y = std::stof(data);
                 }
-                if (i == 3)
+                if (coloumn == 3)
                 {
                     imu.accData[row].z = std::stof(data);
                 }
-                if (i == 4)
+                if (coloumn == 4)
                 {
                     imu.gyroData[row].x = std::stof(data);
                 }
-                if (i == 5)
+                if (coloumn == 5)
                 {
                     imu.gyroData[row].y = std::stof(data);
                 }
-                if (i == 6)
+                if (coloumn == 6)
                 {
                     imu.gyroData[row].z = std::stof(data);
                 }
-                if (i == 7)
+                if (coloumn == 7)
                 {
                     imu.magData[row].x = std::stof(data);
                 }
-                if (i == 8)
+                if (coloumn == 8)
                 {
                     imu.magData[row].y = std::stof(data);
                 }
-                if (i == 9)
+                if (coloumn == 9)
                 {
                     imu.magData[row].z = std::stof(data);
                 }
-                i++;
+                values.erase(0, pos + delimiter.length());
+                coloumn++;
             }
         }
     }
